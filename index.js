8914df482467906bd24d5e9448c3ae3d7fe8eb83
mas-alissa <89016425+mas-alissa@users.noreply.github.com>
2021-09-25 13:43:49 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+//Deel - 1
+
+const age = 20;
+
+if(age >= 18){
+    console.log("This age is greater or equal to 18 years old, You can go inside ")
+} else {
+    console.log("Sorry you can not go inside becouse your age is less than 18 years old")
+}
+
+
+
+//Deel - 2
+
+const isFemale = true;
+
+if(isFemale){
+    console.log("Yes , you are female");
+} else {
+    console.log("No , You are not female");
+}
+
+//Deel - 3
+
+const driverStatus = "bob";
+
+if(driverStatus === "bob"){
+    console.log("Mag rijden")
+} else {
+    console.log("Mag niet rijden")
+}
